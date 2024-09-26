9f47371bc66e28e3c006b2062cc6ac214ffa984d
Vincent Poulailleau <vpoulailleau@gmail.com>
2024-09-26 20:11:43 +0200

    add kingdom cards

--- dopynion/cards.py
+++ dopynion/cards.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import inspect
+import sys
+
 
 class Card:
     name = "Unknown"
@@ -12,36 +15,47 @@ class Card:
         return hash(self.name)
 
 
-class Copper:
+class Copper(Card):
     name = "Cuivre"
     is_kingdom = False
 
 
-class Curse:
+class Curse(Card):
     name = "Malédiction"
     is_kingdom = False
 
 
-class Duchy:
+class Duchy(Card):
     name = "Duché"
     is_kingdom = False
 
 
-class Estate:
+class Estate(Card):
     name = "Domaine"
     is_kingdom = False
 
 
-class Gold:
+class Gold(Card):
     name = "Or"
     is_kingdom = False
 
 
-class Province:
+class Province(Card):
     name = "Province"
     is_kingdom = False
 
 
-class Silver:
+class Silver(Card):
     name = "Argent"
     is_kingdom = False
+
+
+class Smithy(Card):
+    name = "Forgeron"
+
+
+kingdom_cards: list[type[Card]] = [
+    class_
+    for _, class_ in inspect.getmembers(sys.modules[__name__], inspect.isclass)
+    if issubclass(class_, Card) and class_.name != "Unknown" and class_.is_kingdom
+]
