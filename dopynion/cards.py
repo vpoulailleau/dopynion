ee99f6fac3ece88505e0ceefb70bc19c7ec71b73
Vincent Poulailleau <vpoulailleau@gmail.com>
2024-09-26 19:58:23 +0200

    add is_kingdom

--- dopynion/cards.py
+++ dopynion/cards.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 class Card:
     name = "Unknown"
+    is_kingdom = True
 
     def __eq__(self, other: object) -> bool:
         return isinstance(self, other) or self.__class__ is other
@@ -13,27 +14,34 @@ class Card:
 
 class Copper:
     name = "Cuivre"
+    is_kingdom = False
 
 
 class Curse:
     name = "Malédiction"
+    is_kingdom = False
 
 
 class Duchy:
     name = "Duché"
+    is_kingdom = False
 
 
 class Estate:
     name = "Domaine"
+    is_kingdom = False
 
 
 class Gold:
     name = "Or"
+    is_kingdom = False
 
 
 class Province:
     name = "Province"
+    is_kingdom = False
 
 
 class Silver:
     name = "Argent"
+    is_kingdom = False
