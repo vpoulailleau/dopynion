9df41930a44b9bf24a6f3f856ab20ca2559b7201
Vincent Poulailleau <vpoulailleau@gmail.com>
2024-09-26 18:52:36 +0200

    test max players

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,7 +1,19 @@
-from dopynion import Game, Player
+import pytest
+
+from dopynion import Game, InvalidCommandError, Player
 
 
 def test_can_add_player() -> None:
     game = Game()
     game.add_player(Player("player 1"))
     assert len(game.players) == 1
+
+
+def test_max_nb_players() -> None:
+    game = Game()
+    game.add_player(Player("1"))
+    game.add_player(Player("2"))
+    game.add_player(Player("3"))
+    game.add_player(Player("4"))
+    with pytest.raises(InvalidCommandError):
+        game.add_player(Player("5"))
