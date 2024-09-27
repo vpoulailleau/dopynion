37eceb35bb67b4d3ef2c5aa8bba57f97a9dbe73e
Vincent Poulailleau <vpoulailleau@gmail.com>
2024-09-27 19:24:10 +0200

    add states

--- dopynion/player.py
+++ dopynion/player.py
@@ -3,13 +3,26 @@ from random import shuffle
 from dopynion.cards import Card, Copper, Estate
 
 
+class State:
+    ACTION = 1
+    BUY = 2
+    ADJUST = 3
+
+
 class Player:
     def __init__(self, name: str) -> None:
         self.name = name
         self.deck: list[type[Card]] = [Copper] * 7 + [Estate] * 3
-        self.hand: list[type[Card]] = []
         shuffle(self.deck)
+        self.hand: list[type[Card]] = []
+        self.state = State.ACTION
+        self._adjust()
 
     def start_turn(self) -> None:
+        self.state = State.ACTION
+        self._adjust()
+
+    def _adjust(self) -> None:
         self.hand = self.deck[-5:]
         self.deck = self.deck[:-5]
+        self.state = State.ADJUST
