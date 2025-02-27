3813cb55d3e64e04e815e95377063ba76b780a8e
Vincent Poulailleau <vpoulailleau@gmail.com>
2025-02-27 18:07:24 +0100

    test adventurer

--- tests/test_card.py
+++ tests/test_card.py
@@ -2,6 +2,42 @@ from dopynion.cards import CardName
 from dopynion.player import Player
 
 
+def test_adventurer(player_with_action_card: Player) -> None:
+    player = player_with_action_card
+
+    # hand building
+    while player.hand:
+        player.hand.pop()
+    player.hand.append_several(5, CardName.ADVENTURER)
+
+    # deck building
+    while player.deck:
+        player.deck.pop()
+    player.deck.append(CardName.ADVENTURER)
+    player.deck.append(CardName.BUREAUCRAT)
+    player.deck.append(CardName.GOLD)
+    player.deck.append(CardName.COUNCILROOM)
+    player.deck.append(CardName.CURSE)
+    player.deck.append(CardName.SILVER)
+    player.deck.append(CardName.DUCHY)
+    player.deck.append(CardName.ESTATE)
+    player.deck.append(CardName.COPPER)
+    player.deck.append(CardName.SMITHY)
+
+    player.start_turn()
+
+    player.action(CardName.ADVENTURER)
+
+    assert player.discard[0] == CardName.ADVENTURER
+    assert player.discard[1] == CardName.BUREAUCRAT
+    assert player.discard[2] == CardName.COUNCILROOM
+    assert player.discard[3] == CardName.CURSE
+    assert len(player.discard) == 4
+    assert CardName.GOLD in player.hand
+    assert CardName.SILVER in player.hand
+    assert CardName.COPPER not in player.hand
+
+
 def test_festival(player_with_action_card: Player) -> None:
     player = player_with_action_card
     player.hand.append(CardName.FESTIVAL)
